6a2f3b2fe4a267ce5e08d88f0a0dd02bbe0fd4ca
Thorinair <celestek@openmailbox.org>
2015-09-04 18:48:35 +0200

    Ore Dict fixes.

--- main/java/com/celestek/hexcraft/init/HexOreDict.java
+++ main/java/com/celestek/hexcraft/init/HexOreDict.java
@@ -20,34 +20,22 @@ public class HexOreDict {
      */
     public static void registerOres()
     {
-        OreDictionary.registerOre("oreHexoriumRed", new ItemStack(HexBlocks.blockHexoriumNetherOreRed));
-        OreDictionary.registerOre("oreHexoriumRed", new ItemStack(HexBlocks.blockHexoriumNetherMonolithRed));
+        OreDictionary.registerOre("oreNetherHexoriumRed", new ItemStack(HexBlocks.blockHexoriumNetherOreRed));
+        OreDictionary.registerOre("oreNetherHexoriumGreen", new ItemStack(HexBlocks.blockHexoriumNetherOreGreen));
+        OreDictionary.registerOre("oreNetherHexoriumBlue", new ItemStack(HexBlocks.blockHexoriumNetherOreBlue));
+        OreDictionary.registerOre("oreNetherHexoriumWhite", new ItemStack(HexBlocks.blockHexoriumNetherOreWhite));
+        OreDictionary.registerOre("oreNetherHexoriumBlack", new ItemStack(HexBlocks.blockHexoriumNetherOreBlack));
+
         OreDictionary.registerOre("oreHexoriumRed", new ItemStack(HexBlocks.blockHexoriumOreRed));
-        OreDictionary.registerOre("oreHexoriumRed", new ItemStack(HexBlocks.blockHexoriumMonolithRed));
-        OreDictionary.registerOre("gemHexoriumRed", new ItemStack(HexItems.itemHexoriumCrystalRed));
-
-        OreDictionary.registerOre("oreHexoriumGreen", new ItemStack(HexBlocks.blockHexoriumNetherOreGreen));
-        OreDictionary.registerOre("oreHexoriumGreen", new ItemStack(HexBlocks.blockHexoriumNetherMonolithGreen));
         OreDictionary.registerOre("oreHexoriumGreen", new ItemStack(HexBlocks.blockHexoriumOreGreen));
-        OreDictionary.registerOre("oreHexoriumGreen", new ItemStack(HexBlocks.blockHexoriumMonolithGreen));
-        OreDictionary.registerOre("gemHexoriumGreen", new ItemStack(HexItems.itemHexoriumCrystalGreen));
-
-        OreDictionary.registerOre("oreHexoriumBlue", new ItemStack(HexBlocks.blockHexoriumNetherOreBlue));
-        OreDictionary.registerOre("oreHexoriumBlue", new ItemStack(HexBlocks.blockHexoriumNetherMonolithBlue));
         OreDictionary.registerOre("oreHexoriumBlue", new ItemStack(HexBlocks.blockHexoriumOreBlue));
-        OreDictionary.registerOre("oreHexoriumBlue", new ItemStack(HexBlocks.blockHexoriumMonolithBlue));
-        OreDictionary.registerOre("gemHexoriumBlue", new ItemStack(HexItems.itemHexoriumCrystalBlue));
-
-        OreDictionary.registerOre("oreHexoriumWhite", new ItemStack(HexBlocks.blockHexoriumNetherOreWhite));
-        OreDictionary.registerOre("oreHexoriumWhite", new ItemStack(HexBlocks.blockHexoriumNetherMonolithWhite));
         OreDictionary.registerOre("oreHexoriumWhite", new ItemStack(HexBlocks.blockHexoriumOreWhite));
-        OreDictionary.registerOre("oreHexoriumWhite", new ItemStack(HexBlocks.blockHexoriumMonolithWhite));
-        OreDictionary.registerOre("gemHexoriumWhite", new ItemStack(HexItems.itemHexoriumCrystalWhite));
-
-        OreDictionary.registerOre("oreHexoriumBlack", new ItemStack(HexBlocks.blockHexoriumNetherOreBlack));
-        OreDictionary.registerOre("oreHexoriumBlack", new ItemStack(HexBlocks.blockHexoriumNetherMonolithBlack));
         OreDictionary.registerOre("oreHexoriumBlack", new ItemStack(HexBlocks.blockHexoriumOreBlack));
-        OreDictionary.registerOre("oreHexoriumBlack", new ItemStack(HexBlocks.blockHexoriumMonolithBlack));
+
+        OreDictionary.registerOre("gemHexoriumRed", new ItemStack(HexItems.itemHexoriumCrystalRed));
+        OreDictionary.registerOre("gemHexoriumGreen", new ItemStack(HexItems.itemHexoriumCrystalGreen));
+        OreDictionary.registerOre("gemHexoriumBlue", new ItemStack(HexItems.itemHexoriumCrystalBlue));
+        OreDictionary.registerOre("gemHexoriumWhite", new ItemStack(HexItems.itemHexoriumCrystalWhite));
         OreDictionary.registerOre("gemHexoriumBlack", new ItemStack(HexItems.itemHexoriumCrystalBlack));
     }
 }
