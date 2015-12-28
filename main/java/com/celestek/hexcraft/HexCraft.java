2d6c2c87716b9c46e9fd6d525b19255525dcdbec
Thorinair <celestek@openmailbox.org>
2015-12-28 21:06:34 +0100

    Version increment.

--- main/java/com/celestek/hexcraft/HexCraft.java
+++ main/java/com/celestek/hexcraft/HexCraft.java
@@ -22,7 +22,7 @@ public class HexCraft {
     // Set default mod info.
     public static final String MODID = "hexcraft";
     public static final String MODNAME = "HEXCraft";
-    public static final String VERSION = "0.8.3";
+    public static final String VERSION = "0.8.4";
 
     // Set up the instance.
     @Mod.Instance(value = HexCraft.MODID)
