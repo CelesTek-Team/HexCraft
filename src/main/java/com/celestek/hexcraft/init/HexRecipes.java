0431b49d3d860c0ec5586a14576fc26ab5a3b3fa
Thorinair <thorinair@yahoo.com>
2020-06-07 15:47:48 +0200

    Finished all recipes.

--- src/main/java/com/celestek/hexcraft/init/HexRecipes.java
+++ src/main/java/com/celestek/hexcraft/init/HexRecipes.java
@@ -15,80 +15,8 @@ public class HexRecipes {
      * Initializes all recipes and adds them to GameRegistry.
      */
     public static void initializeRecipes() {
-        /* Stack init */
 
-        // Vanilla Blocks
-        ItemStack stackObsidian_1 = new ItemStack(Blocks.obsidian, 1);
-        ItemStack stackStoneButton_1 = new ItemStack(Blocks.stone_button, 1);
-
-        // Vanilla Items
-        ItemStack stackRedstone_1 = new ItemStack(Items.redstone, 1);
-        ItemStack stackIronPickaxe_1 = new ItemStack(Items.iron_pickaxe, 1);
-        ItemStack stackEnderEye_1 = new ItemStack(Items.ender_eye, 1);
-
-        // Cables
-        ItemStack stackHexoriumCableRainbow_1 = new ItemStack(HexBlocks.blockHexoriumCableRainbow, 1);
-
-        // Regular Blocks
-        ItemStack stackHexoriumCoatedStone_1 = new ItemStack(HexBlocks.blockHexoriumCoatedStone, 1);
-
-        // Hex Items
-        // Raw Materials
-        ItemStack stackHexoriumCrystalRed_1 = new ItemStack(HexItems.itemHexoriumCrystalRed, 1);
-        ItemStack stackHexoriumCrystalGreen_1 = new ItemStack(HexItems.itemHexoriumCrystalGreen, 1);
-        ItemStack stackHexoriumCrystalBlue_1 = new ItemStack(HexItems.itemHexoriumCrystalBlue, 1);
-        ItemStack stackHexoriumCrystalWhite_1 = new ItemStack(HexItems.itemHexoriumCrystalWhite, 1);
-        ItemStack stackHexoriumCrystalBlack_1 = new ItemStack(HexItems.itemHexoriumCrystalBlack, 1);
-
-        // Components T1
-        ItemStack stackIronNugget_9 = new ItemStack(HexItems.itemIronNugget, 9);
-        ItemStack stackRainbowEnergizedGlobule_1 = new ItemStack(HexItems.itemRainbowEnergizedGlobule, 1);
-        ItemStack stackWhiteHexoriumWafer_1 = new ItemStack(HexItems.itemWhiteHexoriumWafer, 1);
-        ItemStack stackBlackHexoriumWafer_1 = new ItemStack(HexItems.itemBlackHexoriumWafer, 1);
-        ItemStack stackSpatialProjector_1 = new ItemStack(HexItems.itemSpatialProjector, 1);
-        ItemStack stackOreActuator_1 = new ItemStack(HexItems.itemOreActuator, 1);
-        ItemStack stackPylonSocket_1 = new ItemStack(HexItems.itemPylonSocket, 1);
-        ItemStack stackHexoriumGlassPackage_1 = new ItemStack(HexItems.itemHexoriumGlassPackage, 1);
-
-        // Components T2
-        ItemStack stackRainbowCore_1 = new ItemStack(HexItems.itemRainbowCore, 1);
-        ItemStack stackHexoriumCoprocessor_1 = new ItemStack(HexItems.itemHexoriumCoprocessor, 1);
-        ItemStack stackHexoriumProcessor_1 = new ItemStack(HexItems.itemHexoriumProcessor, 1);
-        ItemStack stackMineralAnalyzer_1 = new ItemStack(HexItems.itemMineralAnalyzer, 1);
-        ItemStack stackHexoriumEnergyConverter_1 = new ItemStack(HexItems.itemHexoriumEnergyConverter, 1);
-        ItemStack stackEnergyInjector_1 = new ItemStack(HexItems.itemEnergyInjector, 1);
-        ItemStack stackMolecularRecoder_1 = new ItemStack(HexItems.itemMolecularRecoder, 1);
-        ItemStack stackHexoriumRFAdapter_1 = new ItemStack(HexItems.itemHexoriumRFAdapter, 1);
-
-        // Components MP
-        ItemStack stackMachineControlPanel_1 = new ItemStack(HexItems.itemMachineControlPanel, 1);
-        ItemStack stackMachineEnergyRouter_1 = new ItemStack(HexItems.itemMachineEnergyRouter, 1);
-        ItemStack stackHeatDissipator_1 = new ItemStack(HexItems.itemHeatDissipator, 1);
-        ItemStack stackHeatingArray_1 = new ItemStack(HexItems.itemHeatingArray, 1);
-        ItemStack stackCrystalExtractionUnit_1 = new ItemStack(HexItems.itemCrystalExtractionUnit, 1);
-        ItemStack stackMatrixAdjuster_1 = new ItemStack(HexItems.itemMatrixAdjuster, 1);
-        ItemStack stackMaterialInteractionPlane_1 = new ItemStack(HexItems.itemMaterialInteractionPlane, 1);
-        ItemStack stackTeleportationFieldProjector_1 = new ItemStack(HexItems.itemTeleportationFieldProjector, 1);
-        ItemStack stackObservationalEye_1 = new ItemStack(HexItems.itemObservationalEye, 1);
-        ItemStack stackDigitalSmartpipe_1 = new ItemStack(HexItems.itemDigitalSmartpipe, 1);
-        ItemStack stackSoundEmissionModule_1 = new ItemStack(HexItems.itemSoundEmissionModule, 1);
-
-        // Tools
-        ItemStack stackHexoriumManipulator_1 = new ItemStack(HexItems.itemHexoriumManipulator, 1);
-        ItemStack stackHexoriumProbe_1 = new ItemStack(HexItems.itemHexoriumProbe, 1);
-        ItemStack stackMolecularTransposer_1 = new ItemStack(HexItems.itemMolecularTransposer, 1);
-        ItemStack stackHexoriumReinforcer_12 = new ItemStack(HexItems.itemHexoriumReinforcer, 12);
-        ItemStack stackMortarPestle_1 = new ItemStack(HexItems.itemMortarPestle, 1);
-        
-        // Dyes
-        ItemStack stackHexoriumDyeBlack_16 = new ItemStack(HexItems.itemHexoriumDyeBlack, 16);
-        ItemStack stackHexoriumDyeWhite_16 = new ItemStack(HexItems.itemHexoriumDyeWhite, 16);
-
-        // Misc
-        ItemStack stackEnergizedHexoriumMonolithBlack_1 = new ItemStack(HexBlocks.blockEnergizedHexoriumMonolithBlack, 1);
-
-
-        /* Recipe init */
+        /* Blocks */
 
         // Gem Blocks
         BlockOfHexoriumCrystal.registerRecipes();
@@ -146,130 +74,10 @@ public class HexRecipes {
         BlockPylonBase.registerRecipes();
         BlockSoundProjector.registerRecipes();
 
-
-        GameRegistry.addSmelting(stackHexoriumCrystalWhite_1, stackWhiteHexoriumWafer_1, 0F);
-        GameRegistry.addSmelting(stackHexoriumCrystalBlack_1, stackBlackHexoriumWafer_1, 0F);
+        /* Items */
 
         // Hex Components
         HexItemComponent.registerRecipes();
-
-        // Components T2
-        GameRegistry.addRecipe(new ShapedOreRecipe(stackRainbowCore_1,
-                " R ",
-                "BAB",
-                " R ",
-                'R', stackRedstone_1, 'A', stackRainbowEnergizedGlobule_1, 'B', "nuggetGold"));
-        GameRegistry.addRecipe(new ShapedOreRecipe(stackHexoriumCoprocessor_1,
-                " R ",
-                "RAR",
-                "BBB",
-                'R', stackRedstone_1, 'A', stackWhiteHexoriumWafer_1, 'B', "nuggetIron"));
-        GameRegistry.addRecipe(new ShapedOreRecipe(stackHexoriumProcessor_1,
-                " R ",
-                "RAR",
-                "BBB",
-                'R', stackRedstone_1, 'A', stackBlackHexoriumWafer_1, 'B', "nuggetGold"));
-        if (HexConfig.cfgSeparatorEnable) {
-            GameRegistry.addRecipe(new ShapedOreRecipe(stackMineralAnalyzer_1,
-                    "BCD",
-                    "FEA",
-                    " R ",
-                    'R', stackRedstone_1, 'A', stackHexoriumCoprocessor_1, 'B', stackHexoriumCrystalRed_1, 'C', stackHexoriumCrystalGreen_1, 'D', stackHexoriumCrystalBlue_1,
-                    'E', "gemDiamond", 'F', "ingotGold"));
-        }
-        GameRegistry.addRecipe(new ShapedOreRecipe(stackHexoriumEnergyConverter_1,
-                "EAE",
-                "CBD",
-                "AEA",
-                'A', stackHexoriumCableRainbow_1, 'B', stackRainbowCore_1, 'C', stackHexoriumProcessor_1, 'D', stackHexoriumCoprocessor_1, 'E', "nuggetIron"));
-        GameRegistry.addRecipe(new ShapedOreRecipe(stackEnergyInjector_1,
-                "CDE",
-                "FBF",
-                " A ",
-                'A', stackHexoriumCableRainbow_1, 'B', stackSpatialProjector_1, 'C', stackHexoriumCrystalRed_1, 'D', stackHexoriumCrystalGreen_1, 'E', stackHexoriumCrystalBlue_1,
-                'F', "nuggetGold"));
-        if (HexConfig.cfgTeleportEnable) {
-            GameRegistry.addRecipe(new ShapedOreRecipe(stackMolecularRecoder_1,
-                    "DRD",
-                    "CBC",
-                    "DAD",
-                    'R', stackRedstone_1, 'A', stackHexoriumCableRainbow_1, 'B', stackHexoriumProcessor_1, 'C', stackRainbowCore_1, 'D', "nuggetIron"));
-        }
-        if (HexConfig.cfgEnergyNodeEnable && HexConfig.cfgEnergyNodePortsRFEnable) {
-            GameRegistry.addRecipe(new ShapedOreRecipe(stackHexoriumRFAdapter_1,
-                    "RBR",
-                    "RAR",
-                    "RBR",
-                    'R', stackRedstone_1, 'A', stackRainbowCore_1, 'B', "nuggetGold"));
-        }
-
-        // Components MP
-        GameRegistry.addRecipe(new ShapedOreRecipe(stackMachineControlPanel_1,
-                "CB ",
-                "CAR",
-                "CB ",
-                'R', stackRedstone_1, 'A', stackHexoriumProcessor_1, 'B', "nuggetGold", 'C', stackStoneButton_1));
-        GameRegistry.addRecipe(new ShapedOreRecipe(stackMachineEnergyRouter_1,
-                "DB",
-                "AR",
-                "DC",
-                'R', stackRedstone_1, 'A', stackHexoriumCableRainbow_1, 'B', stackHexoriumCoprocessor_1, 'C', "nuggetGold", 'D', "nuggetIron"));
-        GameRegistry.addRecipe(new ShapedOreRecipe(stackHeatDissipator_1,
-                "BBR",
-                "BBA",
-                "BBR",
-                'R', stackRedstone_1, 'A', stackHexoriumCrystalWhite_1, 'B', "nuggetIron"));
-        if (HexConfig.cfgFurnaceEnable) {
-            GameRegistry.addRecipe(new ShapedOreRecipe(stackHeatingArray_1,
-                    "RRB",
-                    "RAB",
-                    "RRB",
-                    'R', stackRedstone_1, 'A', stackHexoriumCrystalWhite_1, 'B', "nuggetIron"));
-        }
-        if (HexConfig.cfgSeparatorEnable) {
-            GameRegistry.addRecipe(new ShapedOreRecipe(stackCrystalExtractionUnit_1,
-                    " BD",
-                    "CAD",
-                    " RD",
-                    'R', stackRedstone_1, 'A', stackHexoriumCoprocessor_1, 'B', stackMineralAnalyzer_1, 'C', stackOreActuator_1, 'D', "nuggetIron"));
-        }
-        GameRegistry.addRecipe(new ShapedOreRecipe(stackMatrixAdjuster_1,
-                " ED",
-                "CBA",
-                " RD",
-                'R', stackRedstone_1, 'A', stackHexoriumCoprocessor_1, 'B', stackRainbowCore_1, 'C', stackSpatialProjector_1, 'D', "nuggetIron", 'E', "gemDiamond"));
-        if (HexConfig.cfgTeleportEnable) {
-            GameRegistry.addRecipe(new ShapedOreRecipe(stackMaterialInteractionPlane_1,
-                    "AAA",
-                    "ARA",
-                    "CBC",
-                    'R', stackRedstone_1, 'A', stackHexoriumCrystalBlack_1, 'B', stackHexoriumProcessor_1, 'C', "nuggetGold"));
-            GameRegistry.addRecipe(new ShapedOreRecipe(stackTeleportationFieldProjector_1,
-                    "ADA",
-                    "BCB",
-                    "ERE",
-                    'R', stackRedstone_1, 'A', stackHexoriumCrystalWhite_1, 'B', stackHexoriumCoprocessor_1, 'C', stackSpatialProjector_1, 'D', "gemDiamond", 'E', "nuggetGold"));
-        }
-        if (HexConfig.cfgObserverEnable) {
-            GameRegistry.addRecipe(new ShapedOreRecipe(stackObservationalEye_1,
-                    " A ",
-                    " C ",
-                    " B ",
-                    'A', stackEnergizedHexoriumMonolithBlack_1, 'B', stackHexoriumProcessor_1, 'C', stackEnderEye_1));
-        }
-        if (HexConfig.cfgTankEnable) {
-            GameRegistry.addRecipe(new ShapedOreRecipe(stackDigitalSmartpipe_1,
-                    "ARR",
-                    " DD",
-                    "BCR",
-                    'R', stackRedstone_1, 'A', stackHexoriumCrystalRed_1, 'B', stackHexoriumCrystalGreen_1, 'C', stackHexoriumCoprocessor_1, 'D', "nuggetIron"));
-        }
-        GameRegistry.addRecipe(new ShapedOreRecipe(stackSoundEmissionModule_1,
-                "AEB",
-                "RDR",
-                "FCF",
-                'R', stackRedstone_1, 'A', stackHexoriumCrystalRed_1, 'B', stackHexoriumCrystalBlue_1, 'C', stackHexoriumCrystalGreen_1, 'D', stackHexoriumCrystalWhite_1,
-                'E', stackRainbowEnergizedGlobule_1, 'F', "nuggetIron"));
 
         // Tools
         ItemHexoriumManipulator.registerRecipes();
@@ -282,5 +90,3 @@ public class HexRecipes {
         ItemHexoriumDye.registerRecipes();
     }
 }
-
-// ABCD ABDC ACBD ACDB ADBC ADCB BACD BADC BCAD BCDA BDAC BDCA CABD CADB CBAD CBDA CDAB CDBA DABC DACB DBAC DBCA DCAB DCBA
--- src/main/java/com/celestek/hexcraft/item/HexItemComponent.java
+++ src/main/java/com/celestek/hexcraft/item/HexItemComponent.java
@@ -1,12 +1,15 @@
 package com.celestek.hexcraft.item;
 
 import com.celestek.hexcraft.HexCraft;
+import com.celestek.hexcraft.block.BlockEnergizedHexoriumMonolith;
 import com.celestek.hexcraft.block.BlockHexoriumCable;
 import com.celestek.hexcraft.init.HexBlocks;
 import com.celestek.hexcraft.init.HexConfig;
 import com.celestek.hexcraft.init.HexItems;
 import com.celestek.hexcraft.util.HexEnums;
 import cpw.mods.fml.common.registry.GameRegistry;
+import net.minecraft.block.Block;
+import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
@@ -41,9 +44,14 @@ public class HexItemComponent extends Item {
     }
 
     public static void registerRecipes() {
+        Block cable = HexBlocks.getHexBlock(BlockHexoriumCable.ID, HexEnums.Colors.RAINBOW);
+
         String dr = "dustRedstone";
         String ni = "nuggetIron";
+        String ng = "nuggetGold";
         String ii = "ingotIron";
+        String ig = "ingotGold";
+        String gd = "gemDiamond";
         String gRed = "gemHexoriumRed";
         String gGrn = "gemHexoriumGreen";
         String gBlu = "gemHexoriumBlue";
@@ -93,7 +101,7 @@ public class HexItemComponent extends Item {
                 HexItems.itemPylonSocket,
                 "ICI",
                 "RRR",
-                'R', dr, 'C', HexBlocks.getHexBlock(BlockHexoriumCable.ID, HexEnums.Colors.RAINBOW), 'I', ni));
+                'R', dr, 'C', cable, 'I', ni));
 
         // Hexorium-Glass Package
         GameRegistry.addRecipe(new ShapedOreRecipe(
@@ -102,5 +110,178 @@ public class HexItemComponent extends Item {
                 "LGL",
                 "ILI",
                 'G', "gemHexorium", 'I', ni, 'L', glass));
+
+        /* Components T2 */
+
+        // Rainbow Core
+        GameRegistry.addRecipe(new ShapedOreRecipe(
+                HexItems.itemRainbowCore,
+                " R ",
+                "GEG",
+                " R ",
+                'R', dr, 'E', HexItems.itemRainbowEnergizedGlobule, 'G', ng));
+
+        // Hexorium Coprocessor
+        GameRegistry.addRecipe(new ShapedOreRecipe(
+                HexItems.itemHexoriumCoprocessor,
+                " R ",
+                "RWR",
+                "III",
+                'R', dr, 'W', HexItems.itemWhiteHexoriumWafer, 'I', ni));
+
+        // Hexorium Processor
+        GameRegistry.addRecipe(new ShapedOreRecipe(
+                HexItems.itemHexoriumProcessor,
+                " R ",
+                "RWR",
+                "GGG",
+                'R', dr, 'W', HexItems.itemBlackHexoriumWafer, 'G', ng));
+
+        // Mineral Analyzer
+        if (HexConfig.cfgSeparatorEnable)
+            GameRegistry.addRecipe(new ShapedOreRecipe(
+                    HexItems.itemMineralAnalyzer,
+                    "RGB",
+                    "IDC",
+                    " S ",
+                    'S', dr, 'C', HexItems.itemHexoriumCoprocessor, 'R', gRed, 'G', gGrn, 'B', gBlu, 'D', gd, 'I', ig));
+
+        // Hexorium Energy Converter
+        GameRegistry.addRecipe(new ShapedOreRecipe(
+                HexItems.itemHexoriumEnergyConverter,
+                "ICI",
+                "POS",
+                "CIC",
+                'C', cable, 'O', HexItems.itemRainbowCore, 'P', HexItems.itemHexoriumProcessor, 'S', HexItems.itemHexoriumCoprocessor, 'I', ni));
+
+        // Energy Injector
+        GameRegistry.addRecipe(new ShapedOreRecipe(
+                HexItems.itemEnergyInjector,
+                "RGB",
+                "NSN",
+                " C ",
+                'C', cable, 'S', HexItems.itemSpatialProjector, 'R', gRed, 'G', gGrn, 'B', gBlu, 'N', ng));
+
+        // Molecular Recoder
+        if (HexConfig.cfgTeleportEnable)
+            GameRegistry.addRecipe(new ShapedOreRecipe(
+                    HexItems.itemMolecularRecoder,
+                    "IRI",
+                    "OPO",
+                    "ICI",
+                    'R', dr, 'C', cable, 'P', HexItems.itemHexoriumProcessor, 'O', HexItems.itemRainbowCore, 'I', ni));
+
+        // Hexorium RF Adapter
+        if (HexConfig.cfgEnergyNodeEnable && HexConfig.cfgEnergyNodePortsRFEnable)
+            GameRegistry.addRecipe(new ShapedOreRecipe(
+                    HexItems.itemHexoriumRFAdapter,
+                    "RGR",
+                    "RCR",
+                    "RGR",
+                    'R', dr, 'C', HexItems.itemRainbowCore, 'G', ng));
+
+        /* Components MP */
+
+        // Machine Control Panel
+        GameRegistry.addRecipe(new ShapedOreRecipe(
+                HexItems.itemMachineControlPanel,
+                "BG ",
+                "BPR",
+                "BG ",
+                'R', dr, 'P', HexItems.itemHexoriumProcessor, 'G', ng, 'B', Blocks.stone_button));
+
+        // Machine Energy Router
+        GameRegistry.addRecipe(new ShapedOreRecipe(
+                HexItems.itemMachineEnergyRouter,
+                "IP",
+                "CR",
+                "IG",
+                'R', dr, 'C', cable, 'P', HexItems.itemHexoriumCoprocessor, 'G', ng, 'I', ni));
+
+        // Heat Dissipator
+        GameRegistry.addRecipe(new ShapedOreRecipe(
+                HexItems.itemHeatDissipator,
+                "IIR",
+                "IIG",
+                "IIR",
+                'R', dr, 'G', gWht, 'I', ni));
+
+        // Heating Array
+        if (HexConfig.cfgFurnaceEnable)
+            GameRegistry.addRecipe(new ShapedOreRecipe(
+                    HexItems.itemHeatingArray,
+                    "RRI",
+                    "RGI",
+                    "RRI",
+                    'R', dr, 'G', gWht, 'I', ni));
+
+        // Crystal Extraction Unit
+        if (HexConfig.cfgSeparatorEnable)
+            GameRegistry.addRecipe(new ShapedOreRecipe(
+                    HexItems.itemCrystalExtractionUnit,
+                    " AI",
+                    "OCI",
+                    " RI",
+                    'R', dr, 'C', HexItems.itemHexoriumCoprocessor, 'A', HexItems.itemMineralAnalyzer, 'O', HexItems.itemOreActuator, 'I', ni));
+
+        // Matrix Adjuster
+        GameRegistry.addRecipe(new ShapedOreRecipe(
+                HexItems.itemMatrixAdjuster,
+                " DI",
+                "SOC",
+                " RI",
+                'R', dr, 'C', HexItems.itemHexoriumCoprocessor, 'O', HexItems.itemRainbowCore, 'S', HexItems.itemSpatialProjector, 'I', ni, 'D', gd));
+
+        // Material Interaction Plane
+        if (HexConfig.cfgTeleportEnable)
+            GameRegistry.addRecipe(new ShapedOreRecipe(
+                    HexItems.itemMaterialInteractionPlane,
+                    "GGG",
+                    "GRG",
+                    "NPN",
+                    'R', dr, 'G', gBlk, 'P', HexItems.itemHexoriumProcessor, 'N', ng));
+
+        // Teleportation Field Projector
+        if (HexConfig.cfgTeleportEnable)
+            GameRegistry.addRecipe(new ShapedOreRecipe(
+                    HexItems.itemTeleportationFieldProjector,
+                    "GDG",
+                    "CSC",
+                    "NRN",
+                    'R', dr, 'G', gWht, 'C', HexItems.itemHexoriumCoprocessor, 'S', HexItems.itemSpatialProjector, 'D', gd, 'N', ng));
+
+        // Observational Eye
+        if (HexConfig.cfgObserverEnable)
+            GameRegistry.addRecipe(new ShapedOreRecipe(
+                    HexItems.itemObservationalEye,
+                    "M",
+                    "E",
+                    "P",
+                    'M', HexBlocks.getHexBlock(BlockEnergizedHexoriumMonolith.ID, HexEnums.Colors.BLACK), 'P', HexItems.itemHexoriumProcessor, 'E', Items.ender_eye));
+
+        // Digital Smartpipe
+        if (HexConfig.cfgTankEnable)
+            GameRegistry.addRecipe(new ShapedOreRecipe(
+                    HexItems.itemDigitalSmartpipe,
+                    "ARR",
+                    " II",
+                    "BCR",
+                    'R', dr, 'A', gRed, 'B', gGrn, 'C', HexItems.itemHexoriumCoprocessor, 'I', ni));
+
+        // Sound Emission Module
+        GameRegistry.addRecipe(new ShapedOreRecipe(
+                HexItems.itemSoundEmissionModule,
+                "AGB",
+                "RDR",
+                "ICI",
+                'R', dr, 'A', gRed, 'B', gBlu, 'C', gGrn, 'D', gWht, 'G', HexItems.itemRainbowEnergizedGlobule, 'I', ii));
+
+        // White Hexorium Wafer
+        ItemStack waferWhite = new ItemStack(HexItems.itemWhiteHexoriumWafer, 1);
+        GameRegistry.addSmelting(HexItems.itemHexoriumCrystalWhite, waferWhite, 0.1F);
+
+        // White Hexorium Wafer
+        ItemStack waferBlack = new ItemStack(HexItems.itemBlackHexoriumWafer, 1);
+        GameRegistry.addSmelting(HexItems.itemHexoriumCrystalBlack, waferBlack, 0.1F);
     }
 }
