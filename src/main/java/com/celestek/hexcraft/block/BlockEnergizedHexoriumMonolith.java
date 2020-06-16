f565741c979890ad66182206ac4543cc8c2f4f45
Thorinair <thorinair@yahoo.com>
2020-06-16 18:41:15 +0200

    Add Glowstone to Monolith for glow.

--- src/main/java/com/celestek/hexcraft/block/BlockEnergizedHexoriumMonolith.java
+++ src/main/java/com/celestek/hexcraft/block/BlockEnergizedHexoriumMonolith.java
@@ -1,21 +1,28 @@
 package com.celestek.hexcraft.block;
 
+import coloredlightscore.src.api.CLApi;
 import com.celestek.hexcraft.HexCraft;
 import com.celestek.hexcraft.client.HexClientProxy;
 import com.celestek.hexcraft.client.renderer.HexModelRendererMonolith;
+import com.celestek.hexcraft.init.HexConfig;
 import com.celestek.hexcraft.init.HexItems;
 import com.celestek.hexcraft.util.HexEnums;
 import com.celestek.hexcraft.util.HexUtils;
 import cpw.mods.fml.client.registry.RenderingRegistry;
+import cpw.mods.fml.common.Loader;
 import cpw.mods.fml.common.registry.GameRegistry;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.client.renderer.texture.IIconRegister;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Items;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.AxisAlignedBB;
 import net.minecraft.util.IIcon;
+import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 
 import java.util.ArrayList;
@@ -32,6 +39,7 @@ public class BlockEnergizedHexoriumMonolith extends HexBlockModel implements IHe
     public static final int META_ORIENTATION_0 = 0;
     public static final int META_ORIENTATION_1 = 1;
     public static final int META_ORIENTATION_2 = 2;
+    public static final int META_GLOWING = 3;
 
     // Color
     private final HexEnums.Colors color;
@@ -166,6 +174,22 @@ public class BlockEnergizedHexoriumMonolith extends HexBlockModel implements IHe
     }
 
     /**
+     * Gets the light value according to meta.
+     */
+    @Override
+    public int getLightValue(IBlockAccess world, int x, int y, int z) {
+        if (HexUtils.getMetaBit(META_GLOWING, world, x, y, z))
+            if (Loader.isModLoaded("coloredlightscore")) {
+                float multi = (float) HexConfig.cfgGeneralMonolithGlowstoneLightLevel / 15;
+                return CLApi.makeRGBLightValue(color.r * multi, color.g * multi, color.b * multi);
+            }
+            else
+                return HexConfig.cfgGeneralMonolithGlowstoneLightLevel;
+        else
+            return 0;
+    }
+
+    /**
      * Sets up items to drop.
      */
     @Override
@@ -247,8 +271,13 @@ public class BlockEnergizedHexoriumMonolith extends HexBlockModel implements IHe
             }
         }
         // Return the block (because of Manipulator).
-        else
+        else {
             drops.add(new ItemStack(this, 1));
+
+            // If the monolith had Glowstone added to it.
+            if (HexUtils.getMetaBit(META_GLOWING, world, x, y, z))
+                drops.add(new ItemStack(Items.glowstone_dust, 1));
+        }
 
         // Return the created drop array.
         return drops;
@@ -322,6 +351,36 @@ public class BlockEnergizedHexoriumMonolith extends HexBlockModel implements IHe
             default:
                 return AxisAlignedBB.getBoundingBox((double)x + 0, (double)y + 0, (double)z + 0, (double)x + 1, (double)y + 1, (double)z + 1);
         }
+    }
+
+    /**
+     * Fired when a player right clicks on the pylon.
+     */
+    @Override
+    public boolean onBlockActivated(World world, int x, int y, int z, EntityPlayer player, int par6, float par7, float par8, float par9) {
+        if (!world.isRemote) {
+            boolean isGlowing = HexUtils.getMetaBit(META_GLOWING, world, x, y, z);
+
+            // Check if the item in player's hand is not null and if there is no monolith already inserted.
+            if (player.getCurrentEquippedItem() != null && !isGlowing) {
+                Item item = player.getCurrentEquippedItem().getItem();
+                if (item == Items.glowstone_dust) {
+                    ItemStack stack = player.getCurrentEquippedItem();
+                    stack.stackSize--;
+                    if (stack.stackSize == 0)
+                        stack = null;
+                    player.inventory.setInventorySlotContents(player.inventory.currentItem, stack);
+
+                    HexUtils.setMetaBit(META_GLOWING, true, HexUtils.META_NOTIFY_BOTH, world, x, y, z);
+
+                    return true;
+                }
+            }
+        }
+        else
+            return true;
+
+        return false;
     }
 
     // Prepare the icons.
